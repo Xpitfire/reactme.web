d91a4159342789d969b8036a0585c0a8dac421b3
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2016-12-22 00:26:39 +0100

    Refactored project namespaces

--- src/DorkEdu.Web/Controllers/ValuesController.cs
+++ src/DorkEdu.Web/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication2.Controllers
+namespace DorkEdu.Web.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
