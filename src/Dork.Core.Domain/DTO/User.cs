46bf5e1a1c0668456abf26eac1a453ee77cede2b
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2017-01-21 16:06:26 +0100

    Added domain object basic hashcode and equals implementation

--- src/Dork.Core.Domain/DTO/User.cs
+++ src/Dork.Core.Domain/DTO/User.cs
@@ -11,5 +11,22 @@ namespace Dork.Core.Domain
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public Profile Profile { get; set; }
+        
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this == obj) return true;
+            var user = obj as User;
+            if (user == null) return false;
+            return Username == user.Username;
+        }
+        
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return Username?.GetHashCode() ?? 0;
+            }
+        }
     }
 }
--- src/Dork.Core.Domain/EntityBase.cs
+++ src/Dork.Core.Domain/EntityBase.cs
@@ -8,5 +8,22 @@ namespace Dork.Core.Domain
     public abstract class EntityBase
     {
         public string Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this == obj) return true;
+            var entity = obj as EntityBase;
+            if (entity == null) return false;
+            return Id == entity.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return Id?.GetHashCode() ?? base.GetHashCode();
+            }
+        }
     }
 }
